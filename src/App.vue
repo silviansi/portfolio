f9c8b82b914b4fa7af2e0b99b83b151424139bc8
silviansi <silviansi24@gmail.com>
2025-04-25 18:49:30 +0700

    change template app and add aos animation

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <main class="min-h-screen bg-darkbg text-black font-grotesk">
-    <header v-if="$route.path !== '/'" class="p-6 text-center">
-      <h1 class="text-4xl font-bold text-techy">My Portfolio</h1>
-      <nav class="mt-4 space-x-4">
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </header>
-
-    <router-view />
-  </main>
+  <router-view />
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  AOS.init({
+    duration: 1000,
+    once: true,
+    easing: 'ease-in-out',
+    offset: 50,
+  })
+})
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'aos/dist/aos.css'
 
 createApp(App)
 .use(router)
