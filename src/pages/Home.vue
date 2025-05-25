fc2838d92aad78b989040ea64be5a5e448f072bf
silviansi <silviansi24@gmail.com>
2025-05-25 20:16:10 +0700

    update home section

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,21 +1,21 @@
 <template>
-    <Intro />
-    
-    <main id="home-section" class="bg-black">
+    <main id="home-section" class="bg-white">
+        <Intro />
         
         <!-- Navbar -->
-        <div v-if="showNavbar" class="fixed top-3 left-1/2 transform -translate-x-1/2 z-50 w-full px-4" data-aos="fade-down">
-            <Navbar />
-        </div>
-
-        <!-- Introduction -->
-        <Introduction />
+        <Navbar />
 
         <!-- About -->
         <About />
 
         <!-- Projects -->
         <Project />
+
+        <!-- Certificate-->
+        <Certificate />
+
+        <!-- Contact -->
+        <Contact />
 
         <!-- Footer -->
         <Footer />
@@ -26,34 +26,9 @@
 <script setup>
 import Intro from '@/components/Intro.vue'
 import Navbar from '@/components/Navbar.vue'
-import Introduction  from '@/components/Introduction.vue'
 import About from '@/components/About.vue'
 import Project from '@/components/Project.vue'
+import Certificate from '@/components/Certificate.vue'
+import Contact from '@/components/Contact.vue'
 import Footer from '@/components/Footer.vue'
-import { ref, onMounted, onUnmounted } from 'vue'
-
-// Membuat reactive variable untuk menampilkan navbar
-const showNavbar = ref(false)
-
-const handleScroll = () => {
-    const homeSection = document.getElementById('home-section')
-    const offsetTop = homeSection.offsetTop
-
-    // Menampilkan navbar jika sudah scroll ke bawah
-    showNavbar.value = window.scrollY >= offsetTop - 100
-}
-
-onMounted(() => {
-    window.addEventListener('scroll', handleScroll)
-})
-
-onUnmounted(() => {
-    window.removeEventListener('scroll', handleScroll)
-})
 </script>
-
-<style scoped>
-html {
-    scroll-behavior: smooth;
-}
-</style>
