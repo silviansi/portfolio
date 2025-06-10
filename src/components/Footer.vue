f3b004ad1d866bab92bc898f29efd4af59ae4070
silviansi <silviansi24@gmail.com>
2025-06-10 19:51:21 +0700

    delete text styles

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="bg-white text-gray-800 py-12 mt-24 border-t border-pink-200 font-serif">
+    <footer class="text-gray-800 py-12 mt-24 mb-20 border-t border-pink-200 font-serif">
         <div class="container mx-auto px-6 max-w-4xl text-center">
             <p class="text-xs text-gray-500">
                 Made with <span class="text-red-500">&hearts;</span> by Silvia Nanda Syafa Iswahyudi
