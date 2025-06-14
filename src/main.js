26d37912465158823cf1f6010e1f4dd64e25aed4
silviansi <silviansi24@gmail.com>
2025-06-14 20:26:07 +0700

    import fontawesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'aos/dist/aos.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 createApp(App)
 .use(router)
