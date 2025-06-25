5494d24b3e24cf6073cb9d79c81ac83b4fc38081
silviansi <silviansi24@gmail.com>
2025-06-25 20:44:55 +0700

    refactor router

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DesktopLayout from '@/layouts/DesktopLayout.vue'
 
-// Import komponen yang akan dijadikan halaman
 import Home from '@/pages/Home.vue'
+// import AboutPage from '@/pages/AboutPage.vue'
+// import PathPage from '@/pages/PathPage.vue'
+// import ProjectPage from '@/pages/ProjectPage.vue'
+// import ContactPage from '@/pages/ContactPage.vue'
 
 const routes = [
-  { path: '/', component: Home },
+  {
+    path: '/',
+    component: DesktopLayout,
+    children: [
+      { path: '', name: 'Home', component: Home },
+      // { path: 'about', name: 'About', component: AboutPage },
+      // { path: 'path', name: 'Path', component: PathPage },
+      // { path: 'project', name: 'Project', component: ProjectPage },
+      // { path: 'contact', name: 'Contact', component: ContactPage },
+    ],
+  },
 ]
 
 const router = createRouter({
