33ee88622eea09a04fbb25647b700cc09f7714ac
silviansi <silviansi24@gmail.com>
2025-06-28 20:32:24 +0700

    delete script js

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,3 @@
 <template>
   <router-view />
 </template>
-
-<script setup>
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { onMounted } from 'vue'
-
-onMounted(() => {
-  AOS.init({
-    duration: 1000,
-    once: true,
-    easing: 'ease-in-out',
-    offset: 50,
-  })
-})
-</script>
