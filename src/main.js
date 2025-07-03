73fc6c12fe980df782eefd4eb3bf10bc03743fb6
silviansi <silviansi24@gmail.com>
2025-07-03 20:29:49 +0700

    import pinia

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import 'aos/dist/aos.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
-createApp(App)
-.use(router)
-.mount('#app')
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
